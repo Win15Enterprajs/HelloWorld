76c45aaa9695810638f49de68f342c33cc18ec20
Roppin.L <robin.ljungqvist@gmail.com>
2016-02-23 10:23:04 +0100

    Added Let it snow to the program.

--- HelloWorldAgain/HelloWorldAgain/Program.cs
+++ HelloWorldAgain/HelloWorldAgain/Program.cs
@@ -8,6 +8,23 @@ namespace HelloWorldAgain
 {
     class Program
     {
+        public static void Snow() //// LET IT SNOW!
+        {
+            Board board = new Board();
+            do
+            {
+                Board arr = new Board();
+                int[,] boardArr = new int[20, 20];
+                boardArr = arr.addValues(boardArr);
+                arr.printArr(boardArr);
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    return;
+                }
+                Console.Clear();
+            } while (true);
+        }
         static void Main(string[] args)
         {
             // Adding a line for the Hello world program
